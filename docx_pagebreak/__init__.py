67fb5db7d6a2f8a9f1d644b0add8e1d9658a32a7
riccardovalentinieusphere <136353711+riccardovalentinieusphere@users.noreply.github.com>
2025-04-28 08:49:55 +0200

    Update __init__.py

--- docx_pagebreak/__init__.py
+++ docx_pagebreak/__init__.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" pandoc-docx-pagebreakpy
-Pandoc filter to insert pagebreak as openxml RawBlock
+"""
+pandoc-docx-pagebreakpy
+Pandoc filter to insert pagebreak, sectionbreak and title as openxml RawBlock
 Only for docx output
-
-Trying to port pandoc-doc-pagebreak
-- https://github.com/alexstoick/pandoc-docx-pagebreak
 """
 
 import panflute as pf
 
 
 class DocxPagebreak(object):
-    pagebreak = pf.RawBlock("<w:p><w:r><w:br w:type=\"page\" /></w:r></w:p>", format="openxml")
-    sectionbreak = pf.RawBlock("<w:p><w:pPr><w:sectPr><w:type w:val=\"nextPage\" /></w:sectPr></w:pPr></w:p>",
-                               format="openxml")
+    pagebreak = pf.RawBlock(
+        "<w:p><w:r><w:br w:type=\"page\" /></w:r></w:p>",
+        format="openxml"
+    )
+    sectionbreak = pf.RawBlock(
+        "<w:p><w:pPr><w:sectPr><w:type w:val=\"nextPage\" /></w:sectPr></w:pPr></w:p>",
+        format="openxml"
+    )
     toc = pf.RawBlock(r"""
 <w:sdt>
     <w:sdtContent xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
@@ -31,32 +34,37 @@ class DocxPagebreak(object):
 </w:sdt>
 """, format="openxml")
 
+    def __init__(self):
+        self.title = None
+
     def action(self, elem, doc):
         if isinstance(elem, pf.RawBlock):
-            if elem.text == r"<!--\newpage-->":
-                if (doc.format == "docx"):
+            if elem.text.startswith('<!-- title:'):
+                if doc.format == "docx":
+                    pf.debug("Title detected")
+                    self.title = elem.text.replace('<!-- title:', '').replace('-->', '').strip()
+                    return []
+            elif elem.text == r"<!--\newpage-->":
+                if doc.format == "docx":
                     pf.debug("Page Break")
-                    elem = self.pagebreak
-            # elif elem.text == r"\newsection":
-            #     if (doc.format == "docx"):
-            #         pf.debug("Section Break")
-            #         elem = self.sectionbreak
-            #     else:
-            #         elem = []
+                    return self.pagebreak
             elif elem.text == r"<!--\toc-->":
-                if (doc.format == "docx"):
+                if doc.format == "docx":
                     pf.debug("Table of Contents")
                     para = [pf.Para(pf.Str("Table"), pf.Space(), pf.Str("of"), pf.Space(), pf.Str("Contents"))]
                     div = pf.Div(*para, attributes={"custom-style": "TOC Heading"})
-                    elem = [div, self.toc]
-                else:
-                    elem = []
+                    return [div, self.toc]
         return elem
+
+    def finalize(self, doc):
+        if self.title:
+            header = pf.Header(pf.Str(self.title), level=1)
+            doc.content.insert(0, header)
 
 
 def main(doc=None):
     dp = DocxPagebreak()
-    return pf.run_filter(dp.action, doc=doc)
+    return pf.run_filter(dp.action, prepare=None, finalize=dp.finalize, doc=doc)
 
 
 if __name__ == "__main__":
